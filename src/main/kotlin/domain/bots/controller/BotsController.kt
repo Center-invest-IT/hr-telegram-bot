202eec163039e6512ee0b84a2df8685ab4648f5a
KIRI4444 <kirillpopov6333@gmail.com>
2025-06-24 09:11:24 +0300

    routing refactoring

--- src/main/kotlin/domain/bots/controller/BotsController.kt
+++ src/main/kotlin/domain/bots/controller/BotsController.kt
@@ -2,12 +2,10 @@ package dev.limebeck.openconf.domain.bots.controller
 
 import dev.limebeck.openconf.domain.bots.repository.BotFilter
 import dev.limebeck.openconf.domain.bots.service.BotsService
-import dev.limebeck.openconf.domain.bots.web.APIResponse
 import dev.limebeck.openconf.domain.bots.web.BotStatusDTO
 import dev.limebeck.openconf.domain.bots.web.CreateBotDTO
 import dev.limebeck.openconf.domain.bots.web.mapper.toDTO
 import dev.limebeck.openconf.domain.bots.web.mapper.toEntity
-import dev.limebeck.openconf.domain.bots.web.util.Errors
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.*
@@ -22,39 +20,33 @@ fun Route.botsRouting(
         get {
             try {
                 val bots = botsService.getAllBots().map { it.toDTO() }
-                call.respond(
-                    HttpStatusCode.OK,
-                    APIResponse("Bots found successfully", bots)
-                )
+                call.respond(HttpStatusCode.OK, bots)
             } catch (e: Exception) {
-                call.respond(
-                    HttpStatusCode.Conflict,
-                    APIResponse(Errors.GENERAL, e.message)
-                )
+                call.respond(HttpStatusCode.BadRequest)
             }
         }
 
-        get("/{id}") {
-            val id = call.parameters["id"] ?: kotlin.run {
-                call.respond(HttpStatusCode.BadRequest, APIResponse(Errors.MISS_ID, null))
+        get("/{botId}") {
+            val id = call.parameters["botId"] ?: kotlin.run {
+                call.respond(HttpStatusCode.BadRequest)
                 return@get
             }
 
             try {
                 val bot = botsService.getBotById(id)
                 if (bot == null) {
-                    call.respond(HttpStatusCode.NotFound, APIResponse(Errors.BOT_NOT_FOUND, null))
+                    call.respond(HttpStatusCode.NotFound)
                 } else {
-                    call.respond(HttpStatusCode.OK, APIResponse("Bot found successfully", bot.toDTO()))
+                    call.respond(HttpStatusCode.OK, bot.toDTO())
                 }
             } catch (e: Exception) {
-                call.respond(HttpStatusCode.Conflict, APIResponse(Errors.GENERAL, e.message))
+                call.respond(HttpStatusCode.BadRequest)
             }
         }
 
         get("/search/{username}") {
             val username = call.parameters["username"] ?: kotlin.run {
-                call.respond(HttpStatusCode.BadRequest, APIResponse("Username is required", null))
+                call.respond(HttpStatusCode.BadRequest)
                 return@get
             }
 
@@ -73,15 +65,15 @@ fun Route.botsRouting(
 
             try {
                 val bots = botsService.getBotByUsername(username, filter).map { it.toDTO() }
-                call.respond(HttpStatusCode.OK, APIResponse("Bots found successfully", bots))
+                call.respond(HttpStatusCode.OK, bots)
             } catch (e: Exception) {
-                call.respond(HttpStatusCode.Conflict, APIResponse(Errors.GENERAL, e.message))
+                call.respond(HttpStatusCode.BadRequest)
             }
         }
 
         post {
             val newBot = call.receiveNullable<CreateBotDTO>() ?: kotlin.run {
-                call.respond(HttpStatusCode.BadRequest, APIResponse(Errors.INVALID_BODY, null))
+                call.respond(HttpStatusCode.BadRequest)
                 return@post
             }
 
@@ -93,20 +85,20 @@ fun Route.botsRouting(
                     status = newBot.status.toEntity(),
                     chatId = newBot.chatId
                 )
-                call.respond(HttpStatusCode.OK, APIResponse("Bot added successfully", null))
+                call.respond(HttpStatusCode.Created, newBot)
             } catch (e: Exception) {
-                call.respond(HttpStatusCode.Conflict, APIResponse(Errors.GENERAL, e.message))
+                call.respond(HttpStatusCode.BadRequest)
             }
         }
 
         put("/{id}") {
             val id = call.parameters["id"] ?: kotlin.run {
-                call.respond(HttpStatusCode.BadRequest, APIResponse(Errors.MISS_ID, null))
+                call.respond(HttpStatusCode.BadRequest)
                 return@put
             }
 
             val botDto = call.receiveNullable<CreateBotDTO>() ?: kotlin.run {
-                call.respond(HttpStatusCode.BadRequest, APIResponse(Errors.INVALID_BODY, null))
+                call.respond(HttpStatusCode.BadRequest)
                 return@put
             }
 
@@ -119,27 +111,27 @@ fun Route.botsRouting(
                     status = botDto.status.toEntity(),
                     chatId = botDto.chatId
                 )
-                call.respond(HttpStatusCode.OK, APIResponse("Bot updated successfully", null))
+                call.respond(HttpStatusCode.NoContent)
             } catch (e: NotFoundException) {
-                call.respond(HttpStatusCode.NotFound, APIResponse(Errors.BOT_NOT_FOUND, null))
+                call.respond(HttpStatusCode.NotFound)
             } catch (e: Exception) {
-                call.respond(HttpStatusCode.Conflict, APIResponse(Errors.GENERAL, e.message))
+                call.respond(HttpStatusCode.BadRequest)
             }
         }
 
-        delete("/{id}") {
-            val id = call.parameters["id"] ?: kotlin.run {
-                call.respond(HttpStatusCode.BadRequest, APIResponse(Errors.MISS_ID, null))
+        delete("/{botId}") {
+            val id = call.parameters["botId"] ?: kotlin.run {
+                call.respond(HttpStatusCode.BadRequest)
                 return@delete
             }
 
             try {
                 botsService.deleteBotById(id)
-                call.respond(HttpStatusCode.OK, APIResponse("Bot deleted successfully", null))
+                call.respond(HttpStatusCode.NoContent)
             } catch (e: NotFoundException) {
-                call.respond(HttpStatusCode.NotFound, APIResponse(Errors.BOT_NOT_FOUND, null))
+                call.respond(HttpStatusCode.NotFound)
             } catch (e: Exception) {
-                call.respond(HttpStatusCode.Conflict, APIResponse(Errors.GENERAL, e.message))
+                call.respond(HttpStatusCode.BadRequest)
             }
         }
 
