cce7d210b1e76a725893a4912f117a4856735d9f
KIRI4444 <kirillpopov6333@gmail.com>
2025-07-02 20:27:08 +0300

    Remove APIResponse and Errors

--- src/main/kotlin/domain/bots/web/APIResponse.kt
+++
@@ -1,9 +0,0 @@
-package dev.limebeck.openconf.domain.bots.web
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class APIResponse<T>(
-    val message: String,
-    val data: T?
-)
